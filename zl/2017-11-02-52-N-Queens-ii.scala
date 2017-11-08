8f4c21d3b34e5230c4b15216ea3d7dc3bb442d37
zl <810881283@qq.com>
2017-11-08 11:31:22 +0800

    add two

---
+++ zl/2017-11-02-52-N-Queens-ii.scala
@@ -0,0 +1,54 @@
+object Solution {
+  var res:List[List[String]] = Nil 
+
+  def main(args:Array[String]) ={
+    print(totalNQueens(8))
+
+  }
+  def totalNQueens(n: Int): Int = {
+     val res = solveNQueens(n) 
+     res.size
+  }
+  def solveNQueens(n: Int): List[List[String]]= {
+    res = Nil
+    val queens = Array.ofDim[Int](n,n)
+    solve(queens,0)
+    res
+  }
+
+  def solve(queens:Array[Array[Int]],row:Int):Unit = {
+    val size = queens.length
+    if(row < size){
+      for(i <- 0 until size){
+        if(valid(queens,row,i)){
+          queens(row)(i) = 1
+          solve(queens,row+1)
+          queens(row)(i) = 0
+        }
+      }
+    }else{
+      res = res :+ queens
+        .map(_.map{
+          case 0 => "."
+          case 1 => "Q"
+        })
+        .map(_.mkString(""))
+        .toList
+    }
+  }
+  def valid(arr: Array[Array[Int]], row: Int, col: Int):Boolean = {
+    var res = true
+    val size = arr.length
+    for(i <- 0 until row) {
+      if ( col - (row - i) >= 0 && col - (row - i) < size && arr(i)(col - (row - i)) == 1) {
+        res = false
+      } else if ( col + (row - i) >= 0 && col + (row - i) < size && arr(i)(col + (row - i)) == 1) {
+        res = false
+      }
+      else if (arr(i)(col) == 1) {
+        res = false
+      }
+    }
+    res
+  }
+}
